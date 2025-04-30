c7311463270d89ca3799ace0d60a233613aa4fc8
SoufianeEddiry <36312828+SoufianeEddiry@users.noreply.github.com>
2025-04-30 15:59:52 +0100

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-ktPCorUhqH11CNTRL-GYYUP8WctGgmqkEHEeKUHgY7KYmmFYuqf
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kQ7Zgmubx911CNTRL-4Di8DGFDVpQqUfA4UsF7qQpynJ7t2YQe
